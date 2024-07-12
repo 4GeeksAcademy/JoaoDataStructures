4852c2a9ae6f270d4dec04657c8ebaefde810035
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-07-12 19:35:08 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -5,12 +5,28 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
+from datastructures import FamilyStructure,FamilyMember
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+
+ribeiroFamily = FamilyStructure("Ribeiro")
+myPeopleArray = [FamilyMember(0,"Joao", 29, [0,1,2,3,4,5,6]),
+                 FamilyMember(0,"Mike", 29, [0,1,2,3,4,5,6]),
+                 FamilyMember(0,"Ajay", 29, [0,1,2,3,4,5,6]),
+                 FamilyMember(0,"Deepak", 29, [0,1,2,3,4,5,6])]
+
+
+for people in myPeopleArray:
+        ribeiroFamily.add_member(people)
+
+for member in ribeiroFamily._members:
+        print(member)
+
+ribeiroFamily.delete_member(1)
+     
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,26 +11,47 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self.id = 1
 
         # example list of members
         self._members = []
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
-        return randint(0, 99999999)
+        return randint(0, 10)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member.last_name = self.last_name
+        # member.id = self._generateId()
+        member.id = self.id
+        self.id=self.id + 1
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            print(member.id)
+            if(member.id == id):
+                self._members.remove(member)
+            else: return print("ID not found")
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if(member.id == id):
+                
+                return member
+            else: return print("ID not found" + member.id)
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+class FamilyMember:
+    last_name = None
+    def __init__(self, id, firstName, age, luckyNumbers):
+        self.id = id
+        self.firstName = firstName
+        self.age = age
+        self.luckyNumbers = luckyNumbers
+        
+    def __str__(self):
+        return f"{self.id}{self.firstName} {self.age} {self.last_name}"
